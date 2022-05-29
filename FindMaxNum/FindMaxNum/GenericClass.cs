0fd9240c3894ed3f22cb8192d383cf4bac04cdaf
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-29 21:45:54 +0530

    [Madhavi] added code for finding maximum interger

---
+++ FindMaxNum/FindMaxNum/GenericClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    public class GenericClass
+    {
+        public int MaxInt(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            else if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+            {
+                return secondNum;
+            }
+            else
+            {
+                return thirdNum;
+            }
+        }
+    }
+}
---
+++ FindMaxNum/FindMaxNum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsProgram
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            var log = NLog.LogManager.GetCurrentClassLogger();
+            log.Info("Running Generics Program");
+
+            GenericClass accessMethod = new GenericClass();
+            var forInt = accessMethod.MaxInt(500, 750, 200);
+            Console.WriteLine("The Maximum Integer Value is: " + forInt);
+        }
+    }
+}
